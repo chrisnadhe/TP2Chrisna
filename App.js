97221d54c99a053ff6274e29cd56e9be12e51dc5
chrisnadhe <40015276+chrisnadhe@users.noreply.github.com>
2024-01-07 20:29:10 +0700

    create navigation bar

--- App.js
+++ App.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import HomeScreen from './src/HomeScreen'; // Import your HomeScreen component
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import { Provider } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+import HomeScreen from './src/HomeScreen';
+import ProfileScreen from './src/ProfileScreen';
+import MapsScreen from './src/MapsScreen';
+
+const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
-  return <HomeScreen />; // Render the HomeScreen component
+  return (
+    <SafeAreaProvider>
+      <Provider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="cog" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Maps"
+              component={MapsScreen}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="cog" color={color} size={26} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-const HomeScreen = () => {
+function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Selamat datang pada aplikasi ini</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
