48502f59948ce32feafbdcf13a9dea8646dc64d4
chrisnadhe <40015276+chrisnadhe@users.noreply.github.com>
2024-01-13 05:10:12 +0700

    add maps screen

--- src/MapsScreen.js
+++ src/MapsScreen.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Platform, PermissionsAndroid, PermissionsIOS } from 'react-native';
-import MapView, { PROVIDER_OSM } from 'react-native-maps';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+  PermissionsIOS,
+} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Config from "react-native-config";
+
+const apiKey = Config.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 function MapsScreen() {
+  const [isLoading, setIsLoading] = useState(true);
+
   const [initialRegion, setInitialRegion] = useState({
     latitude: -6.17511,
     longitude: 106.82725,
@@ -13,9 +25,9 @@ function MapsScreen() {
   const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
   useEffect(() => {
-    requestLocationPermission().then(hasPermission => {
-      setHasLocationPermission(hasPermission);
-    });
+    requestLocationPermission()
+      .then(setHasLocationPermission)
+      .finally(() => setIsLoading(false));
   }, []);
 
   const requestLocationPermission = async () => {
@@ -24,16 +36,21 @@ function MapsScreen() {
       return status === 'authorized';
     } else {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       );
       return granted === PermissionsAndroid.RESULTS.GRANTED;
     }
   };
 
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <MapView
-        provider={PROVIDER_OSM}
+        provider={PROVIDER_GOOGLE}
+        apiKey={apiKey}
         style={styles.map}
         initialRegion={initialRegion}
         showsUserLocation={hasLocationPermission}
